23c9fe2e49b15f82cbbc439b0a00c5bf6893a353
Jean <jean-marco-10@hotmail.com>
2020-05-19 21:24:20 -0600

    Create action and update working

--- src/api/scoreEndpoints.js
+++ src/api/scoreEndpoints.js
@@ -4,6 +4,14 @@ export const scoreRequest = (id, userId) => {
   return config.get(`/scores/${id}`, { params: { user_id: userId } });
 };
 
+export const createScoreRequest = (data) => {
+  return config.post("scores", data);
+};
+
+export const updateScoreRequest = (id, data) => {
+  return config.put(`scores/${id}`, data);
+};
+
 export const riskFactorsRequest = () => {
   return config.get("/risk_factors/");
 };
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,16 +3,18 @@ import authReducer from "./modules/auth/auth";
 import clientReducer from "./modules/auth/client";
 import eventsReducer from "./modules/events/eventsReducer";
 import filterReducer from "./modules/filters/filters";
+import questionReducer from "./modules/questions/questionList";
+import scoreReducer from "./modules/score/score";
 import sessionReducer from "./modules/auth/session";
 import usersReducer from "./modules/users/userList";
-import questionReducer from "./modules/questions/questionList";
 
 export default combineReducers({
   authReducer,
   clientReducer,
   eventsReducer,
   filterReducer,
+  questionReducer,
+  scoreReducer,
   sessionReducer,
   usersReducer,
-  questionReducer,
 });
